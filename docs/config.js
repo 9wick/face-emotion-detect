9d292a2281fd4668705eaec58d3675a776956178
木戸 康平 <bouningenn@gmail.com>
2019-02-11 20:46:30 +0900

    change to int

--- docs/config.js
+++ docs/config.js
@@ -1,14 +1,2 @@
 var faceEmotionCondition = [
-  {
-    "video": "nuJHenDGUa8",
-    "emotion": {
-      "happy": {">": 0.6},
-    }
-  },
-  {
-    "video": "QJgHMc8GGSc",
-    "emotion": {
-      "sad": {">": 0.4},
-    }
-  }
 ]
